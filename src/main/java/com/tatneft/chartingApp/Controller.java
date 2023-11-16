15095eb0194d3a5136ba1eb3d9d0f7e25c9e7470
TisheVody <lesvtornikutro@yandex.ru>
2023-11-16 18:28:09 +0300

    added images, polarChart

--- src/main/java/com/tatneft/chartingApp/Controller.java
+++ src/main/java/com/tatneft/chartingApp/Controller.java
@@ -1,15 +1,22 @@
 package com.tatneft.chartingApp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.SubScene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.control.Label;
-import java.awt.Color;
+
+import java.awt.*;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
@@ -24,21 +31,42 @@ import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.chart.title.TextTitle;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class Controller implements Initializable {
-    @FXML
-    private SubScene subScene;
-    @FXML
-    private BarChart<?, ?> mainChart;
-    @FXML
-    private AnchorPane chartAnc;
 
     @FXML
-    private Label welcomeText;
+    private ToggleButton btnAreaChart;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ToggleGroup btnGroup;
+
+    @FXML
+    private ToggleButton btnPolarChart;
+
+    @FXML
+    private ToggleButton btnSankeyChart;
+
+    @FXML
+    private ImageView imgExample;
+    @FXML
+    private Label lbl;
+
+    public void toggleButtonSelect (ActionEvent event) {
+        ImageView image = null;
+        if (btnAreaChart.isSelected()) {
+            image = new ImageView("C:\\Users\\trust\\IdeaProjects\\chartingApp\\src\\main\\resources\\images\\image_area_chart.png");
+        }
+        if (btnPolarChart.isSelected()) {
+            image = new ImageView("C:\\Users\\trust\\IdeaProjects\\chartingApp\\src\\main\\resources\\images\\image_polar_chart.png");
+        }
+        if (btnSankeyChart.isSelected()) {
+            image = new ImageView("C:\\Users\\trust\\IdeaProjects\\chartingApp\\src\\main\\resources\\images\\image_sankey_chart.png");
+        }
+        assert image != null;
+        imgExample.setImage(image.getImage());
     }
 
     private static CategoryDataset createDataset() {
@@ -78,6 +106,52 @@ public class Controller implements Initializable {
         return chart;
     }
 
+    private static XYSeries createRandomData(final String name, final double baseRadius, final double thetaInc) {
+        final XYSeries series = new XYSeries(name);
+        for (double coeff = 0.0; coeff < 365.0; coeff += thetaInc) {
+            final double radius = baseRadius * (1.1 + Math.random());
+            series.add(coeff, radius);
+        }
+        return series;
+    }
+    private static XYSeriesCollection createPolarDataset() {
+
+        XYSeriesCollection dataSet = new XYSeriesCollection();
+        XYSeries series1 = createRandomData("Series 1", 77.0, 8.0);
+        XYSeries series2 = createRandomData("Series 2", 52.0, 6.0);
+        XYSeries series3 = createRandomData("Series 3", 32.0, 2.0);
+        dataSet.addSeries(series1);
+        dataSet.addSeries(series2);
+        dataSet.addSeries(series3);
+        return dataSet;
+    }
+
+    /**
+     * Creates a sample chart.
+     *
+     * @param dataSet  the dataset.
+     *
+     * @return The chart.
+     */
+    private static JFreeChart createPolarChart(XYDataset dataSet) {
+        JFreeChart chart = ChartFactory.createPolarChart("Polar Chart Example", dataSet, true, true, false);
+        chart.addSubtitle(new TextTitle("Time to generate 1000 charts in SVG "
+                + "format (lower bars = better performance)"));
+        chart.setBackgroundPaint(Color.white);
+        //CategoryPlot plot = chart.getCategoryPlot();
+
+        // set transparent background paint, so the background color of the parent is used
+//        java.awt.Color transparent = new java.awt.Color(1.0f, 1.0f, 1.0f, 0f);
+//        chart.setBackgroundPaint(transparent);
+
+//        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+//        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+//        BarRenderer renderer = (BarRenderer) plot.getRenderer();
+        //renderer.setDrawBarOutline(false);
+        chart.getLegend().setFrame(BlockBorder.NONE);
+        return chart;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         CategoryDataset dataset = createDataset();
@@ -85,6 +159,11 @@ public class Controller implements Initializable {
         ChartFrame frame = new ChartFrame("BarChart,,,,,,,,,", chart);
         frame.setVisible(true);
         frame.setSize(800, 600);
-        //ChartViewer viewer = new ChartViewer(chart);
+
+        XYSeriesCollection dataSet = createPolarDataset();
+        JFreeChart chart1 = createPolarChart(dataSet);
+        ChartFrame frame1 = new ChartFrame("BarChart,,,,,,,,,", chart1);
+        frame1.setVisible(true);
+        frame1.setSize(800, 600);
     }
 }
